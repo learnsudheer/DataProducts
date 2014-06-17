53fa9c6bafe7bc75eb9b1aa338fe8250027c3f45
learnsudheer <learnsudheer@gmail.com>
2014-06-17 18:04:08 -0400

    Update UI.R

--- UI.R
+++ UI.R
@@ -4,6 +4,13 @@ shinyUI(
     # Application Title
     headerPanel("Diabetes Prediction Algorithm"),
     sidebarPanel(
+      h5('Read me: This form does nothing, it gathers patients information and prints.'),
+      h5('1. Enter Patients Name'),
+      h5('2. Enter Food Habits'),
+      h5('3. Enter Gender'),
+      h5('4. Enter Glucose level'),
+      h5('5. Click on Submit'),
+      h5('-----------------------------------------'),
       textInput('name', label = h5("Patient Name")),
       checkboxGroupInput("foodH",label = h5("Food Habits"),
                          c("Veggies" = "Veggies",
@@ -14,12 +21,11 @@ shinyUI(
       radioButtons("gender", label = h5("Gender"),
                    choices = list("Male" = "Male", "Female" = "Female"), selected ='null'),
       numericInput('glucose',label = h5("Glucose mg/dl"), 0, min = 50, max = 200, step = 5),
-      submitButton('Submit Prediction') 
+      submitButton('Submit') 
     ),
     
     mainPanel(
-        h5('Read me: This form gathers, patients Name, Food Habits, Gender and Glucose level. Please enter all the fields and hit submit'),
-    
+      
         h3('Patient Details'),      
         h4('Name: '),
         verbatimTextOutput("patientName"),
